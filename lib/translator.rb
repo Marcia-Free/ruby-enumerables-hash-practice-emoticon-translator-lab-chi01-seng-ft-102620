4a26aa13f8383f5d69be93f57352748e78075174
Marcia Freeman <marciafreeman2011@gmail.com>
2020-10-16 18:38:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,9 +32,9 @@ end
 def get_english_meaning(file_path, emoticon)
   translated_lib = load_library(file_path)
   
-  #eng_meaning = translated_lib['get_meaning'][emoticon]
-  #eng_meaning ? eng_meaning : "Sorry, that emoticon was not found"
-  #binding.pry
+  eng_meaning = translated_lib['get_meaning'][emoticon]
+  eng_meaning ? eng_meaning : "Sorry, that emoticon was not found"
+  binding.pry
 end
 
 
