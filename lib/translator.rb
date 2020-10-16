7ab71451e82600c7e622ad952f3559ce8ad50ff1
Marcia Freeman <marciafreeman2011@gmail.com>
2020-10-16 18:09:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,8 +15,8 @@ def load_library(file_path)
     english = value[0]
     japanese = value[1]
     
-   #key_lib[meaning] = {}
-   #key_lib[meaning] = {}
+   key_lib[meaning] = {}
+   key_lib[meaning] = {}
    key_lib[meaning][:english] = english
    key_lib[meaning][:japanese] = japanese
     
@@ -26,22 +26,25 @@ def load_library(file_path)
   end
   #translated_lib
   key_lib
-
- #binding.pry
 end
+
 
 
 def get_japanese_emoticon(file_path, emoticon)
-  #translated_lib = load_library(file_path)
+  key_lib = load_library(file_path)
   
-  #jap_emoticon = translated_lib["get_emoticon"][emoticon]
+  #jap_emoticon = key_lib[meaning][:japanese][]
   #jap_emoticon ? jap_emoticon : "Sorry, that emoticon was not found"
+  
 end
 
 
+
+
 def get_english_meaning(file_path, emoticon)
-  #translated_lib = load_library(file_path)
+  key_lib = load_library(file_path)
   
-  #eng_meaning = translated_lib["get_meaning"][emoticon]
-  #eng_meaning ? eng_meaning : "Sorry, that emoticon was not found"
+  eng_meaning = key_lib["get_meaning"][emoticon]
+  eng_meaning ? eng_meaning : "Sorry, that emoticon was not found"
+  binding.pry
 end
